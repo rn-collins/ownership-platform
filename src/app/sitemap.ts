e00df8b06aa5000afd4132b261e0df7776741bf7
rn-collins <rayven.nikkita.collins@gmail.com>
2026-07-26 21:17:06 -1000

    Add Edition 002 to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -14,6 +14,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     "/findings",
     "/edit",
     "/edit/001",
+    "/edit/002",
     "/partner",
     "/about",
     "/research/cognitive-interviews",

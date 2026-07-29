7d61abe47c9dc3f5c5119c869814c69aae31f145
rn-collins <rayven.nikkita.collins@gmail.com>
2026-07-28 15:06:29 -1000

    Exclude unpublished experiments from indexing

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -13,6 +13,8 @@ export default function robots(): MetadataRoute.Robots {
         "/research/admin",
         "/research/observatory",
         "/observatory/guest/",
+        "/observatory/resources",
+        "/observatory/teaching",
         "/login",
         "/auth/",
       ],
